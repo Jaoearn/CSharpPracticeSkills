2e2d93557cf5cc93c00a8382eb9e4386aa41cd68
jaoearn <jaoearn.sp@gmail.com>
2023-04-25 16:57:32 +0700

    Min Max

---
+++ MinMax/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter integer:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            List<int> num = new List<int>();
+
+            while (input > 0)
+            {
+                Console.WriteLine("Please enter another integer (enter 0 for cal): ");
+                input = Convert.ToInt32(Console.ReadLine());
+                num.Add(input);
+            }
+
+            int max = num.Max();
+            int min = num.Where(x => x != 0).Min();
+            Console.WriteLine("Max Number is " + max);
+            Console.WriteLine("Min Number is " + min);
+        }
+    }
+}
