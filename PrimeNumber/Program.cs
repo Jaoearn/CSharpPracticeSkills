b8edcba007c7b47d5ab413d5c568bdbb0ec957ce
jaoearn <jaoearn.sp@gmail.com>
2023-04-25 19:15:11 +0700

    PrimeNumber

--- PrimeNumber/Program.cs
+++ PrimeNumber/Program.cs
@@ -6,7 +6,24 @@ namespace PrimeNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            if(input == 2 || input == 3 || input == 5 || input == 7 || input == 11 || input == 19)
+            {
+                Console.WriteLine("Number is Prime.");
+            }
+            else if (input % 2 == 0 || input % 3 == 0 || input % 7 == 0)
+            {
+                Console.WriteLine("Number is not Prime.");
+            }
+            else if (input % 2 == 1)
+            {
+                Console.WriteLine("Number is Prime.");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Prime.");
+            }
         }
     }
 }
